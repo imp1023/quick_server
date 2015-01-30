22db91eb4ab9094c63baa4266d8caaac6a8f1d44
CrazyPro <dplxl@163.com>
2015-01-30 10:17:34 +0800

    modify start_quick_server.sh

--- start_quick_server.sh
+++ start_quick_server.sh
@@ -1,20 +1,38 @@
-CURRDIR=$(dirname $(readlink -f $0))
-NGINX_DIR=$CURRDIR/openresty/nginx/
-SRCDIR=$CURRDIR/openresty/server/actions/tools/
+#!/bin/bash
 
-$CURRDIR/redis/bin/redis-server $CURRDIR/conf/redis.conf
+function help()
+{
+	echo "Usage: [sudo] ./start_quick_server.sh [OPTIONS] [--debug]"
+	echo "Options:"
+	echo "\t -a | --all \t\t start nginx, redis and beanstalkd"
+	echo "\t -n | --nginx \t\t start nginx"
+	echo "\t -b | --beanstalkd \t\t start beanstalkd"
+	echo "\t -r | --redis \t\t start redis"
+	echo "\t -h | --help \t\t show help"
+	echo "if OPTIONS is not specified, default OPTION is \"-a(--all)\"."
+	echo "in default, server will start in release mode, or else it will start in debug mode if you specified \"--debug\" following OPTIONS"
+	echo "But NOTICE that \"--debug\" dont take effect except in OPTIONS \"-a(--all)\" and \"-n(--nginx)\""
+}
 
-service mysqld start
+help()
 
-cd $NGINX_DIR
-. ./sbin/start.sh
+#CURRDIR=$(dirname $(readlink -f $0))
+#NGINX_DIR=$CURRDIR/openresty/nginx/
+#SRCDIR=$CURRDIR/openresty/server/actions/tools/
 
-cd $SRCDIR
-. ./cleaner.sh  > /dev/null 2> /opt/.cleaner.log &
+#$CURRDIR/redis/bin/redis-server $CURRDIR/conf/redis.conf
 
-cd $CURRDIR
+#service mysqld start
 
- 
+#cd $NGINX_DIR
+#. ./sbin/start.sh
+
+#cd $SRCDIR
+#. ./cleaner.sh  > /dev/null 2> /opt/.cleaner.log &
+
+#cd $CURRDIR
+
+
 
 
 
