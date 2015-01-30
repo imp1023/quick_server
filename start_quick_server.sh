901955f8f7a288f3f993fe37006a9f76fe187321
CrazyPro <dplxl@163.com>
2015-01-30 11:52:07 +0800

    modify start.sh

--- start_quick_server.sh
+++ start_quick_server.sh
@@ -6,7 +6,7 @@ function help()
 	echo "Options:"
 	echo -e "\t -a | --all \t\t start nginx, redis and beanstalkd"
 	echo -e "\t -n | --nginx \t\t start nginx"
-	echo -e "\t -b | --beanstalkd \t\t start beanstalkd"
+	echo -e "\t -b | --beans \t\t start beanstalkd"
 	echo -e "\t -r | --redis \t\t start redis"
 	echo -e "\t -h | --help \t\t show help"
 	echo "if OPTIONS is not specified, default OPTION is \"-a(--all)\"."
